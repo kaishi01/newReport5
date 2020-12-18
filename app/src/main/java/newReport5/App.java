a9fdedb95b25fbfa62de93bd452195a8dd2eb7e1
kaishi <e205746@ie.u-ryukyu.ac.jp>
2020-12-18 19:57:29 +0900

    step2

--- app/src/main/java/newReport5/App.java
+++ app/src/main/java/newReport5/App.java
@@ -6,6 +6,13 @@ package newReport5;
 public class App {
     public static void main(String[] args) {
         String str = null;
-        System.out.println(str.length());
+        try {
+            System.out.println(str.length());
+
+        } catch (NullPointerException e) {
+            System.out.println("NullPointerException発生");
+            System.out.println(e.getMessage());
+        }
+
     }
 }
